0c2942f488a7c57695e969d534daa4d30acb5e75
anthonyspeicher <anthony@speicher.org>
2026-01-19 15:50:18 -0500

    Fixed JPEG SOI bug, created custom hashing function for sets

--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 * https://en.wikipedia.org/wiki/Exif
 * https://www.fileformat.info/format/jpeg/egff.htm
 * https://www.ozhiker.com/electronics/pjmt/jpeg_info/app_segments.html
+* https://www.libpng.org/pub/png/spec/1.2/PNG-Chunks.html
+* https://en.wikipedia.org/wiki/PNG
+* https://stackoverflow.com/questions/8026890/c-how-to-insert-array-into-hash-set
--- scrubber.cpp
+++ scrubber.cpp
@@ -2,18 +2,43 @@
 /// 1/13/2026
 /// An EXIF metadata scrubber
 
+#include <cstring>
 #include <cstdio>
 #include <cstdlib>
 #include <cstdint>
+#include <unordered_set>
+#include <array>
+#include <functional>
 
-/// CURRENTLY ONLY SUPPORTS JPEG IMAGE TYPES
+
+/// add documentation :P
+void parse_JPEG(FILE* input, FILE* output);
+
+
+void parse_PNG(FILE* input, FILE* output);
+
+
+/// custom hash algorithm for unordered_set compatability
+/// thanks to fredoverflow on https://stackoverflow.com/questions/8026890/c-how-to-insert-array-into-hash-set
+
+struct Array_Hash {
+  size_t operator() (const std::array<uint8_t, 4>& arr) const {
+    std::hash<uint8_t> hasher;
+    size_t seed = 0;
+    for (uint8_t i : arr) {
+      /// hash using magic constant
+      seed ^= hasher(i) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+    }
+    return seed;
+  }
+};
 
 int main (int argc, const char *argv[]) {
   /// instead of writing in place, EXIF block is completely removed
 
   /// validate usage ----------------------------------------------------------------------------------
   if (argc < 2 || argc > 3) {
-    fprintf(stderr, "Incorrect argument format. Usage: ./a.out filename.jpg cleaned.jpg (default: cleaned_image.jpg)\n");
+    fprintf(stderr, "Incorrect argument format. Usage: ./a.out filename.jpg cleaned.jpg (default: cleaned_image)\n");
     exit(1);
   }
 
@@ -21,18 +46,39 @@ int main (int argc, const char *argv[]) {
   FILE* input = std::fopen(argv[1], "rb");
   if (!input) {
     perror("Error opening input file");
-    printf("Usage: ./a.out filename.jpg cleaned.jpg (default: cleaned_image.jpg)\n");
+    printf("Usage: ./a.out filename.jpg cleaned.jpg (default: cleaned_image)\n");
     exit(1);
   }
-  FILE* output = std::fopen(argc == 3 ? argv[2] : "cleaned_image.jpg", "wb");
+  FILE* output = std::fopen(argc == 3 ? argv[2] : "cleaned_image", "wb");
   if (!output) {
     perror("Error opening output file");
-    printf("Usage: ./a.out filename.jpg cleaned.jpg (default: cleaned_image.jpg)\n");
+    printf("Usage: ./a.out filename.jpg cleaned.jpg (default: cleaned_image)\n");
     exit(1);
   }
 
+  /// determine filetype and parse --------------------------------------------------------------------
+  uint8_t buffer[8];
+  uint8_t png_marker[8] = {0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A};
+  fread(buffer, 1, 8, input);
+
+  if (buffer[0] == 0xFF && buffer[1] == 0xD8) {         /// check for JPEG SOI
+    parse_JPEG(input, output);
+  } else if (memcmp(buffer, png_marker, 8*sizeof(uint8_t)) == 0) {        /// for PNG
+    fwrite(buffer, 1, 8, output);
+    parse_PNG(input, output);
+  } else {
+    fprintf(stderr, "File corrupted or not of recognized type\n");
+  }
+
+
+  /// exit cleanup ------------------------------------------------------------------------------------
+  exit(0);
+}
+
+
+void parse_JPEG(FILE* input, FILE* output) {
   uint8_t buffer[2];
-  uint16_t len;
+  size_t len;
 
   /// find length until 0xFF 0xD9 (EOI marker, may be metadata after) ----------------------------------
   if (fseek(input, 0, SEEK_END) != 0) {
@@ -48,13 +94,21 @@ int main (int argc, const char *argv[]) {
   /// using 1kb chunks to reduce fread calls, reverse search data for EOI marker
   uint8_t temp_buf[1024];
   bool found = 0;
+  size_t chunk_size;
+
   while (!found && file_length > 0) {
-    file_length -= 1024;
+    if (file_length >= 1024) {
+      file_length -= 1024;
+      chunk_size = 1024;
+    } else {
+      chunk_size = file_length;
+      file_length = 0;
+    }
     fseek(input, file_length, SEEK_SET);
-    fread(temp_buf, 1, 1024, input);
+    fread(temp_buf, 1, chunk_size, input);
 
     /// search temp_buf for EOI
-    for (size_t i(1022); i >= 0; i--) {
+    for (int i = chunk_size - 2; i >= 0; i--) {
       if (temp_buf[i] == 0xFF && temp_buf[i + 1] == 0xD9) {
         found = 1;
         file_length += i;
@@ -70,14 +124,8 @@ int main (int argc, const char *argv[]) {
   }
 
   rewind(input);
-
-  /// checks SOI segment to validate data -------------------------------------------------------------
   fread(buffer, 1, 2, input);
-  if (!(buffer[0] == 0xFF && buffer[1] == 0xD8)) {
-    printf("%d %d\n", buffer[0], buffer[1]);
-    fprintf(stderr, "File corrupted or not of type JPEG.\n");
-    exit(1);
-  } else fwrite(buffer, 1, 2, output);
+  fwrite(buffer, 1, 2, output);
 
   /// loop until start of SOS segment (picture data) to remove all APP metadata -----------------------
   while (fread(buffer, 1, 2, input) == 2 && buffer[1] != 0xDA) {
@@ -114,13 +162,23 @@ int main (int argc, const char *argv[]) {
   uint8_t buf[256];
   file_length -= ftell(input);
 
-  while (file_length) {
+  if (file_length < 0) {
+    fprintf(stderr, "Verbatim copy failed\n");
+    exit(1);
+  }
+
+  while (file_length > 0) {
     len = file_length > 256 ? 256 : file_length;
     fread(buf, 1, len, input);
     fwrite(buf, 1, len, output);
     file_length -= len;
   }
+}
 
-  /// exit cleanup ------------------------------------------------------------------------------------
-  exit(0);
+void parse_PNG(FILE* input, FILE* output) {
+  /// NOTE TO SELF - remove tEXt, zTXt, iTXt, eXIf, tIME, and pHYs segments
+  std::unordered_set<std::array<uint8_t, 4>, Array_Hash> metadata_headers; /// add headers later
+
+  uint8_t buffer[4];
+  uint8_t data[256];
 }
